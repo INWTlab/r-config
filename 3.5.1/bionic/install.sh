d3e46d938be3896ea1a9e736d826985d9181c1d5
Sebastian Warnholz <wahani@users.noreply.github.com>
2018-08-11 10:26:27 +0200

    Update install.sh

--- 3.5.1/bionic/install.sh
+++ 3.5.1/bionic/install.sh
@@ -2,8 +2,8 @@
 ##   URL: https://github.com/INWTlab/r-docker/blob/master/r-ver-ubuntu/Dockerfile
 ##   to be used for VMs instead of Docker images.
 
-export BUILD_DATE="2018-07-30"
-export R_VERSION="3.5.1"
+BUILD_DATE="2018-07-30"
+R_VERSION="3.5.1"
 # export LC_ALL="en_US.UTF-8"
 # export LANG="en_US.UTF-8"
 
@@ -49,17 +49,21 @@ sudo make install
 # -- end --
 ## Add a library directory (for user-installed packages)
 sudo mkdir -p /usr/local/lib/R/${R_VERSION}/lib/R/site-library
-sudo chown root:staff /usr/local/lib/R/${R_VERSION}/lib/R/site-library
-sudo chmod g+wx /usr/local/lib/R/${R_VERSION}/lib/R/site-library
 ## Fix library path
-# echo "R_LIBS_USER='/usr/local/lib/R/${R_VERSION}/site-library'" >> /usr/local/R/${R_VERSION}/lib/etc/Renviron
-echo "R_LIBS=\${R_LIBS-'/usr/local/lib/R/${R_VERSION}/site-library'}" >> /usr/local/lib/R/${R_VERSION}/etc/Renviron
+echo "R_LIBS_USER=\${R_LIBS_USER-'~/R/x86_64-pc-linux-gnu-library/${R_VERSION}'}" >> /usr/local/lib/R/${R_VERSION}/lib/R/etc/Renviron
+echo "R_LIBS=\${R_LIBS-'/usr/local/lib/R/${R_VERSION}/lib/R/site-library:/usr/local/lib/R/${R_VERSION}/lib/R/library'}" >> /usr/local/lib/R/${R_VERSION}/lib/R/etc/Renviron
 ## install packages from date-locked MRAN snapshot of CRAN
 [ -z "$BUILD_DATE" ] && BUILD_DATE=$(TZ="America/Los_Angeles" date -I) || true
 MRAN=https://mran.microsoft.com/snapshot/${BUILD_DATE}
-sudo echo MRAN=$MRAN >> /etc/environment
-export MRAN=$MRAN
-sudo echo "options(repos = c(CRAN='$MRAN'), download.file.method = 'libcurl')" >> /usr/local/lib/R-${R_VERSION}/etc/Rprofile.site
+sudo echo "options(repos = c(CRAN='$MRAN'), download.file.method = 'libcurl')" >> /usr/local/lib/R/${R_VERSION}/lib/R/etc/Rprofile.site
+
+# Make this version the default
+sudo ln -s /usr/local/lib/R/${R_VERSION}/lib/R/bin/R /usr/local/bin/R
+sudo ln -s /usr/local/lib/R/${R_VERSION}/lib/R/bin/Rscript /usr/local/bin/Rscript
+# Make version available
+sudo ln -s /usr/local/lib/R/${R_VERSION}/lib/R/bin/R /usr/local/bin/R-${R_VERSION}
+sudo ln -s /usr/local/lib/R/${R_VERSION}/lib/R/bin/Rscript /usr/local/bin/Rscript-${R_VERSION}
+
 ## Clean up from R source install
 cd ~
 rm -rf /tmp/R-${R_VERSION}
