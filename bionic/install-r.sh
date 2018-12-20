3ea32bef256d44930f25570b0551ebba6b337cd1
Sebastian Warnholz <wahani@users.noreply.github.com>
2018-12-20 15:11:08 +0100

    Update install-r.sh

--- bionic/install-r.sh
+++ bionic/install-r.sh
@@ -12,7 +12,7 @@ apt-get update
 apt-get build-dep r-base
 apt-get install curl libssl-dev libxml2-dev libssh2-1-dev openjdk-8-jdk \
         libbz2-dev liblzma-dev libpcre3-dev libpng-dev libgdal-dev \
-        libglu1-mesa-dev freeglut3-dev mesa-common-dev
+        libglu1-mesa-dev freeglut3-dev mesa-common-dev sed
 # apt-get install libmysqlclient-dev
 
 ## -- BUILD R --
@@ -54,12 +54,8 @@ make install
 # -- end --
 ## Add a library directory (for user-installed packages)
 mkdir -p /usr/local/lib/R/${R_VERSION}/lib/R/site-library
-## Fix library path
-echo "R_LIBS_USER='~/R/x86_64-pc-linux-gnu-library/${R_VERSION}'" >> /usr/local/lib/R/${R_VERSION}/lib/R/etc/Renviron
-echo "R_LIBS=\${R_LIBS-'/usr/local/lib/R/${R_VERSION}/lib/R/site-library:/usr/local/lib/R/${R_VERSION}/lib/R/library'}" >> /usr/local/lib/R/${R_VERSION}/lib/R/etc/Renviron
-echo ".libPaths('/usr/local/lib/R/${R_VERSION}/lib/R/library')" >> /usr/local/lib/R/${R_VERSION}/lib/R/etc/Rprofile.site
-echo ".libPaths('/usr/local/lib/R/${R_VERSION}/lib/R/site-library')" >> /usr/local/lib/R/${R_VERSION}/lib/R/etc/Rprofile.site
-echo ".libPaths('~/R/x86_64-pc-linux-gnu-library/${R_VERSION}')" >> /usr/local/lib/R/${R_VERSION}/lib/R/etc/Rprofile.site
+## Set user library to version specific
+sed -i "s/^R_LIBS_USER.*/R_LIBS_USER=\${R_LIBS_USER-'~\/R\/x86_64-pc-linux-gnu-library\/$R_VERSION'}/" /usr/local/lib/R/3.5.1/lib/R/etc/Renviron
 ## install packages from date-locked MRAN snapshot of CRAN
 [ -z "$BUILD_DATE" ] && BUILD_DATE=$(TZ="America/Los_Angeles" date -I) || true
 MRAN=https://mran.microsoft.com/snapshot/${BUILD_DATE}
