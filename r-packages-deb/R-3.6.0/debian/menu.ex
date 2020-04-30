c40475b8dca57f8ef17e4ca24225d9ee29b6eba4
aneudecker <andreas.neudecker@inwt-statistics.de>
2020-04-30 15:46:44 +0200

    move dir

---
+++ r-packages-deb/R-3.6.0/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(r-3.6.0):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="r-3.6.0" command="/usr/bin/r-3.6.0"
---
+++ r-packages-deb/build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+
+R_VERSION=${R_VERSION:-3.6.0}
+MRAN_TIMESTAMP=${MRAN:-2019-12-12}
+PACKAGE_VERSION=${PACKAGE_VERSION:-1.0.0}
+UBUNTU_RELEASE=${UBUNTU_RELEASE:-eoan}
+
+MRAN=https://mran.microsoft.com/snapshot/${MRAN_TIMESTAMP}
+
+# dh_make -y \
+#   --native \
+#   --single \
+#   --packagename r-$R_VERSION-packages_$PACKAGE_VERSION \
+#   --email andreas.neudecker@inwt-statistics.de
+
+# add r-3.6.0 as dependency to debian/control
+# add debian/install: files/opt/* opt
+
+mkdir -p files/opt/R/$R_VERSION/lib/x86_64-linux-gnu/R/library
+Rscript installPackages.R $MRAN files/opt/R/$R_VERSION/lib/x86_64-linux-gnu/R/library
+
+RELEASE=$(lsb_release -sc)
+
+dpkg-buildpackage --no-sign
+
+mkdir -p /tmp/deb-repo/$UBUNTU_RELEASE/amd64
+
+mv ../*.deb /tmp/deb-repo/$UBUNTU_RELEASE/amd64
+
+cd /tmp/deb-repo/$UBUNTU_RELEASE
+
+apt-ftparchive --arch amd64 packages amd64 > Packages
+gzip -k -f Packages
+
+apt-ftparchive release . > Release
+
+# sign Release file
+gpg --import --passphrase-file ~/.gpg/passphrase \
+    --batch --pinentry-mode loopback ~/.gpg/private.key
+
+gpg -abs --passphrase-file ~/.gpg/pgp_passphrase -o Release.gpg \
+    --batch --pinentry-mode loopback Release
